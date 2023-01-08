abfb7ae9449093b55944138642e9e1161778b9b5
Simrche <simon.roche@edu.devinci.fr>
2023-01-08 11:37:01 +0100

    championship card component

--- components/championship.vue
+++ components/championship.vue
@@ -12,18 +12,7 @@
                     </div>
                 </div>
 
-                <div class="bg-white w-full flex justify-between items-center py-2 rounded-xl px-8 mt-2" v-for="(player, index) in 4" :key="index">
-                    <div class="flex items-center">
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">1.</h3>
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">Simon</h3>
-                    </div>
-
-                    <div class="flex w-2/5">
-                    <h3 class="text-lg w-1/3 text-center">10€</h3>
-                    <h3 class="text-lg w-1/3 text-center">1.30</h3>
-                    <h3 class="text-lg w-1/3 text-center text-red-600">10€</h3>
-                    </div>
-                </div>
+                <ChampionshipCard :index="index" v-for="(player, index) in 4" :key="index"></ChampionshipCard>
             </b-tab-item>
 
             <b-tab-item label="Mois">
@@ -35,18 +24,7 @@
                     </div>
                 </div>
 
-                <div class="bg-white w-full flex justify-between items-center py-2 rounded-xl px-8 mt-2" v-for="(player, index) in 4" :key="index">
-                    <div class="flex items-center">
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">1.</h3>
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">Simon</h3>
-                    </div>
-
-                    <div class="flex w-2/5">
-                    <h3 class="text-lg w-1/3 text-center">10€</h3>
-                    <h3 class="text-lg w-1/3 text-center">1.30</h3>
-                    <h3 class="text-lg w-1/3 text-center text-red-600">10€</h3>
-                    </div>
-                </div>
+                <ChampionshipCard :index="index" v-for="(player, index) in 4" :key="index"></ChampionshipCard>
             </b-tab-item>
 
             <b-tab-item label="All Time">
@@ -58,18 +36,7 @@
                     </div>
                 </div>
 
-                <div class="bg-white w-full flex justify-between items-center py-2 rounded-xl px-8 mt-2" v-for="(player, index) in 4" :key="index">
-                    <div class="flex items-center">
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">1.</h3>
-                    <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">Simon</h3>
-                    </div>
-
-                    <div class="flex w-2/5">
-                    <h3 class="text-lg w-1/3 text-center">10€</h3>
-                    <h3 class="text-lg w-1/3 text-center">1.30</h3>
-                    <h3 class="text-lg w-1/3 text-center text-red-600">10€</h3>
-                    </div>
-                </div>
+                <ChampionshipCard :index="index" v-for="(player, index) in 4" :key="index"></ChampionshipCard>
             </b-tab-item>
         </b-tabs>
     </section>
---
+++ components/championshipCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-white w-full flex justify-between items-center py-2 rounded-xl px-8 mt-2">
+        <div class="flex items-center">
+            <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">1.</h3>
+            <h3 class="ml-4 text-lg" :class="{'text-yellow-400': index === 0, 'text-gray-400': index === 1, 'text-amber-800': index === 2}">Simon</h3>
+        </div>
+
+        <div class="flex w-2/5">
+            <h3 class="text-lg w-1/3 text-center">10€</h3>
+            <h3 class="text-lg w-1/3 text-center">1.30</h3>
+            <h3 class="text-lg w-1/3 text-center text-red-600">10€</h3>
+        </div>
+    </div>
+</template>
+
+<script setup lang=ts>
+    defineProps<{
+        index: number
+    }>()
+</script>
