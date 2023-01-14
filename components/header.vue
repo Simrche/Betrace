9b9de1bb4a18117973ca95637e5567433f93204a
Simrche <simon.roche@edu.devinci.fr>
2023-01-15 00:07:25 +0100

    class bold on navigation button

--- components/header.vue
+++ components/header.vue
@@ -3,9 +3,9 @@
         <h1 class="text-3xl font-bold">Betrace</h1>
 
         <ul class="gap-x-6 ml-16 <md:gap-x-2 <md:ml-2 flex items-center <sm:hidden">
-          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'bets')">Paris</li>
-          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'championship')">Classements</li>
-          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'hof')">Hall of Fame</li>
+          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'bets')" :class="{'font-bold': tab === 'bets'}">Paris</li>
+          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'championship')" :class="{'font-bold': tab === 'championship'}">Classements</li>
+          <li class="text-lg ml-20 <md:text-md <md:ml-2 cursor-pointer" @click="$emit('redirection', 'hof')" :class="{'font-bold': tab === 'hof'}">Hall of Fame</li>
         </ul>
 
         <div class="fixed bg-white w-full h-16 bottom-0 hidden justify-around <sm:flex z-10">
